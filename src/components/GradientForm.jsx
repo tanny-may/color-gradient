c255dddf70e56da7a12b54b33a69f24369107653
Tanya <tanya777fan@mail.ru>
2024-08-30 10:58:58 +0300

    useState changed

--- src/components/GradientForm.jsx
+++ src/components/GradientForm.jsx
@@ -1,69 +1,64 @@
 import styles from './GradientForm.module.css';
 import { useState } from 'react';
 
+const inputs = {
+	inputValue1: '',
+	inputValue2: '',
+	inputValue3: '',
+};
+
 // eslint-disable-next-line react/prop-types
 export function GradientForm({ width, height }) {
-	const [color1, setColor1] = useState('white');
-	const [color2, setColor2] = useState('black');
-	const [inputValue1, setinputValue1] = useState('');
-	const [inputValue2, setinputValue2] = useState('');
-
-	// const input1 = useRef();
-	// function clearInputs() {
-	// 	console.log(input1);
-	// 	input1.current.value = '';
-	// }
+	const [background, setBackground] = useState('white');
+	const [inputValues, setInputValues] = useState(inputs);
 
 	const wrapperStyles = {
 		width: width,
 		height: height,
-		background: `linear-gradient(${color1}, ${color2})`,
+		background,
 	};
 
 	function clearInputs() {
-		setinputValue1('');
-		setinputValue2('');
+		setInputValues(inputs);
+		setBackground('white');
 	}
 
 	function handleSubmit(e) {
 		e.preventDefault();
-		setColor1(inputValue1);
-		setColor2(inputValue2);
+		setBackground(`linear-gradient(${Object.values(inputValues).join(', ')}`);
 	}
 
+	function handleChange(e) {
+		setInputValues({ ...inputValues, [e.target.name]: e.target.value });
+	}
 
 	return (
 		<div className={styles.wrapper} style={wrapperStyles}>
 			<form onSubmit={handleSubmit}>
 				<label className={styles.inputs}>
-					<input
-						// ref={input1}
-						name='color1'
-						className={styles.input}
-						value={inputValue1}
-						minLength='4'
-						maxLength='7'
-						onChange={(e) => {setinputValue1(e.target.value)}}
-					></input>
-					<input
-						name='color2'
-						className={styles.input}
-						value={inputValue2}
-						minLength='4'
-						maxLength='7'
-						onChange={(e) => {setinputValue2(e.target.value)}}
-					></input>
+					{Object.keys(inputValues).map((el, i) => {
+
+						return (
+							<input
+								key={i}
+								name={el}
+								className={styles.input}
+								value={inputValues[el]}
+								onChange={handleChange}
+							></input>
+						);
+					})}
 				</label>
 
-				<button className={styles.button} type='submit'>
-					PAINT
-				</button>
-				<button className={styles.button} type='button' onClick={clearInputs}>
-					CLEAR
-				</button>
+				<div className={styles.buttons}>
+					<button className={styles.button} type='submit'>
+						PAINT
+					</button>
+					<button className={styles.button} type='button' onClick={clearInputs}>
+						CLEAR
+					</button>
+				</div>
 			</form>
 		</div>
 	);
 }
-
-
